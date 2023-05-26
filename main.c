4ef1aa8d9880204219d1ee2991bf0c0fd0287d55
josefe <jf.gomez@upm.es>
2023-05-26 14:30:38 +0200

    Corrected unprocedent disclaimer

--- main.c
+++ main.c
@@ -1,53 +1,9 @@
-/*****************************************************************************
-*                           main.c
-*
-* Demo for DMX Controller
-*
-*
-* Hardware:
-*  Lighting Communications Motherboard DM160214
-*  Lighting Communications DMX512 Adapter board AC160214-2
-*
-*
-* CODE OWNERSHIP AND DISCLAIMER OF LIABILITY
-*
-* Microchip Technology Incorporated ("Microchip") retains all ownership and
-* intellectual property rights in the code accompanying this message and in
-* all derivatives hereto.  You may use this code, and any derivatives created
-* by any person or entity by or on your behalf, exclusively with Microchips
-* proprietary products.  Your acceptance and/or use of this code constitutes
-* agreement to the terms and conditions of this notice.
-*
-* CODE ACCOMPANYING THIS MESSAGE IS SUPPLIED BY MICROCHIP "AS IS".  NO
-* WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT
-* LIMITED TO, IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND
-* FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS CODE, ITS INTERACTION WITH
-* MICROCHIPS PRODUCTS, COMBINATION WITH ANY OTHER PRODUCTS, OR USE IN ANY
-* APPLICATION.
-*
-* YOU ACKNOWLEDGE AND AGREE THAT, IN NO EVENT, SHALL MICROCHIP BE LIABLE,
-* WHETHER IN CONTRACT, WARRANTY, TORT (INCLUDING NEGLIGENCE OR BREACH OF
-* STATUTORY DUTY), STRICT LIABILITY, INDEMNITY, CONTRIBUTION, OR OTHERWISE,
-* FOR ANY INDIRECT, SPECIAL, PUNITIVE, EXEMPLARY, INCIDENTAL OR CONSEQUENTIAL
-* LOSS, DAMAGE, FOR COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
-* CODE, HOWSOEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
-* POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT ALLOWABLE
-* BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS
-* CODE, SHALL NOT EXCEED THE PRICE YOU PAID DIRECTLY TO MICROCHIP SPECIFICALLY
-* TO HAVE THIS CODE DEVELOPED.
-*
-* You agree that you are solely responsible for testing the code and
-* determining its suitability.  Microchip has no obligation to modify, test,
-* certify, or support the code.
-*
-* Author            Date            Ver     Comment
-*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-* Michael Pearce    23 October 2012 0.00    Write demo code
-*
-* Michael Pearce   5 November 2012  0.01    Add Color mixing option.
-*                                           Clear first 4 channels when modes
-*                                           are changes. 
-******************************************************************************/
+/*
+ * File:   main.c
+ * Author: josefe
+ *
+ * Created on June 13, 2017, 1:55 PM
+ */
 
 
 #include "dmx.h"
